689b4e05be0e1bc1c76fab86c7f9aeb61d837b23
Peter Rupnik <peter.rupnik@ijs.si>
2024-11-12 15:08:50 +0100

    Wrap project in a python package.

---
+++ src/tg_reader/__init__.py
@@ -0,0 +1,70 @@
+from praatio.utilities.constants import Interval
+from pathlib import Path
+from loguru import logger
+import pandas as pd
+
+
+def tg_to_events(inpath: str | Path, target_tier: int = 3) -> list[Interval]:
+    from praatio import textgrid
+
+    tg = textgrid.openTextgrid(inpath, includeEmptyIntervals=False)
+    logger.debug(f"Tier {target_tier} = '{tg.tierNames[target_tier]}' is selected")
+    results = list(tg.tiers[target_tier])
+    for this in results:
+        for other in results:
+            if this == other:
+                continue
+            if (this.start <= other.end) and (this.end >= other.end):
+                raise ValueError("Events overlap!")
+    return results
+
+
+def events_to_frames(
+    events: list[Interval],
+    default_label=None,
+    max_time: float = -6.66,
+    min_time: float = 0.0,
+) -> list[str]:
+    """Transforms textgrid Intervals into a list of labels.
+
+    :param list[Interval] events: Intervals with attrib end, start, and label[str]
+    :param _type_ default_label: What to use in frames where no Interval is found, defaults to None
+    :return list[str]:
+    """
+    import pandas as pd
+
+    assert max_time > min_time
+    frames = pd.interval_range(start=min_time, end=max_time, freq=0.02)
+    labels = [default_label for i in frames]
+    for i, frame in enumerate(frames):
+        for event in events:
+            es, ee = event.start, event.end
+            fs, fe = frame.left, frame.right
+            if (es <= fe) and (ee >= fs):
+                labels[i] = event.label
+    return labels
+
+
+def frames_to_intervals(frames: list[int]) -> list[pd.Interval]:
+    raise NotImplementedError(
+        "This part has not yet been adapted to multi-target labels. Go yell at the developer."
+    )
+    return_list = []
+    ndf = pd.DataFrame(
+        data={
+            "millisecond": [20 * i for i in range(len(frames))],
+            "frames": frames,
+        }
+    )
+
+    ndf["millisecond"] = ndf.millisecond.astype(int)
+    ndf = ndf.dropna()
+    indices_of_change = ndf.frames.diff()[ndf.frames.diff() != 0].index.values
+    for si, ei in pairwise(indices_of_change):
+        if ndf.loc[si : ei - 1, "frames"].mode()[0] == 0:
+            pass
+        else:
+            return_list.append(
+                pd.Interval(ndf.loc[si, "millisecond"], ndf.loc[ei - 1, "millisecond"])
+            )
+    return return_list
---
+++ tests/test_on_sample_files.py
@@ -0,0 +1,33 @@
+import pytest
+import tg_reader as tr
+
+from pathlib import Path
+
+test_file = Path("sample_data/fgDDtwU4we4_15533.0-15538.76.TextGrid")
+
+
+def test_event_generation():
+    events = tr.tg_to_events(test_file)
+    assert len(events) == 25
+    assert events[0].label == "+"
+
+
+def test_frame_generation():
+    events = tr.tg_to_events(test_file)
+    default_label = None
+    max_time = max([i.end for i in events])
+
+    frames = tr.events_to_frames(events, default_label=None, max_time=max_time)
+    assert set(frames) == set([i.label for i in events]).union(
+        {default_label}
+    ), "Sets of labels differ between frames and events..."
+    assert len(frames) == int(50 * max_time)
+    with pytest.raises(AssertionError):
+        tr.events_to_frames(
+            events, default_label=None, max_time=max_time, min_time=max_time + 1
+        )
+
+
+def test_not_implemented():
+    with pytest.raises(NotImplementedError):
+        tr.frames_to_intervals(frames=[0, 0, 1])
